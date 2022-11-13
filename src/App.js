e6c33b082d9e238d4d2fed42ee8268b120f5900e
AnonymousAnybodyElse <anonymousanybodyelse@gmail.com>
2022-11-13 10:00:05 +0800

    PWA manifest

--- src/App.js
+++ src/App.js
@@ -48,10 +48,7 @@ function App() {
                 咨询热线：023-12345
             </div>
             <div className={'footer-wrapper-shit'}>
-                数据来源： 依托全国一体化政务服务平台实现跨省（区、
-            </div>
-            <div className={'footer-wrapper-shit-end'}>
-                市）数据共享和互通互认
+                数据来源： 依托全国一体化政务服务平台实现跨省（区、市）数据共享和互通互认
             </div>
         </div>
     </div>
