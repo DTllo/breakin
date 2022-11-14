a85f799fb9b10f239f45fbfb623625f042265eb0
AnonymousAnybodyElse <anonymousanybodyelse@gmail.com>
2022-11-15 00:31:22 +0800

    优化加载速度

--- src/scan_result.js
+++ src/scan_result.js
@@ -5,6 +5,8 @@ import ScanResultRight from './assets/scan_result_right.png';
 import ScanBtns from './assets/scan_btns.png';
 import ScanBg from './assets/scan_bg.png';
 import { Outlet, Link } from "react-router-dom";
+import Check24 from './assets/24_check.png';
+import showImg from './assets/show.png';
 function ScanResult(){
     let scanBtnRef = useRef();
     let scanResultRef = useRef();
@@ -60,11 +62,11 @@ function ScanResult(){
     }
 
     function AddHistory(){
-        let area = prompt('请输入所在大区', '江北区');
+        let area = prompt('场所行政区划', '江北区');
         if(area !== null && area !== "") {
-            let road = prompt('请输入所在街道', '莎莎街道');
+            let road = prompt('场所详细地址', '莎莎街道');
             if(road !== null && road !== "") {
-                let location = prompt('请输入详细地址', '莎莎一号舞厅91号入口');
+                let location = prompt('场所名称', '莎莎一号舞厅91号入口');
                 if(location !== null && location !== "") {
                     let result = {};
                     result.area = area;
@@ -123,25 +125,28 @@ function ScanResult(){
             <div className={'scan-result-wrapper'}>
                 <img className={'scan-bg'} src={ScanBg} alt={''}/>
                 <div className={'scan-result-img-wrapper'} style={{paddingBottom:btnHeight + 15}}>
-                    <img className={'scan-result-img'} src={ScanResultImg} alt={''} ref={scanResultRef}/>
+                    {/*<img className={'scan-result-img'} src={ScanResultImg} alt={''} ref={scanResultRef}/>*/}
+                    <ScanPageCard setShowListFnc={setShowList} position={{location:location,area:area,road:road}}/>
                     <div className={'scan-result-right-wrapper'}  style={{top: resultHeight / 2 - rightHeight / 2}}>
                         <img className={'scan-result-right anima'} src={ScanResultRight} alt={''} ref={RightRef}/>
+                        <div className={'scan-result-right-text'}>绿码</div>
                     </div>
-                    <div className={'scan-time'} style={{top:resultHeight / 8 * 2 + 40}}>{time}</div>
-                    <div className={'scan-location-location'}>{location}</div>
-                    <div className={'scan-location-area'}>重庆市/{area}</div>
-                    <div className={'scan-location-road'}>{road}</div>
-                    <div className={'scan-edit-location'} style={{height:resultHeight / 4}} onClick={()=>{
-                        setShowList(true)
-                    }}></div>
+                    {/*<div className={'scan-time'} style={{top:resultHeight / 8 * 2 + 40}}>{time}</div>*/}
+                    {/*<div className={'scan-location-location'}>{location}</div>*/}
+                    {/*<div className={'scan-location-area'}>重庆市/{area}</div>*/}
+                    {/*<div className={'scan-location-road'}>{road}</div>*/}
+                    {/*<div className={'scan-edit-location'} style={{height:resultHeight / 4}} onClick={()=>{*/}
+                    {/*    setShowList(true)*/}
+                    {/*}}></div>*/}
                 </div>
             </div>
-            <img className={'scan-btns'} src={ScanBtns} alt={''} ref={scanBtnRef}/>
-            <div className={'scan-btns-click'} style={{height:btnHeight}}>
-                <Link to={'/yukang_code_fake/'}><div className={'scan-btn-to-qr'}></div></Link>
+            {/*<img className={'scan-btns'} src={ScanBtns} alt={''} ref={scanBtnRef}/>*/}
+            <ScanPageBottomBtnSlots reference={scanBtnRef}/>
+            {/*<div className={'scan-btns-click'} style={{height:btnHeight}}>*/}
+            {/*    <Link to={'/yukang_code_fake/'}><div className={'scan-btn-to-qr'}></div></Link>*/}
 
-                <div></div>
-            </div>
+            {/*    <div></div>*/}
+            {/*</div>*/}
             {
                 showList && <div className={'history-list-wrapper'}>
                     <div className={'history-list-bg'} onClick={()=>{
@@ -167,7 +172,7 @@ function ScanResult(){
                                                     <div className={'history-item-delete'} onClick={(e)=>{
                                                         // console.log(value.id)
                                                         DeleteItem(index);
-                                                    }}>❌</div>
+                                                    }}>-</div>
                                                 </div>
                                             )
                                         })
@@ -181,6 +186,127 @@ function ScanResult(){
             }
 
         </>
+    )
+}
+
+function ScanPageBottomBtnSlots(props){
+    let { reference } = props;
+    return(
+        <div className={'scan-page-bottom-btn-slots'} ref={reference}>
+            <ScanPageBottomBtnGoBackToYuKangPage/>
+            <ScanPageBottomBtnOpenXingChengPopup/>
+        </div>
+    )
+}
+
+function ScanPageBottomBtnGoBackToYuKangPage(){
+    return(
+        <Link className={'spb-back-yk-page'} to={'/yukang_code_fake/'}>
+            <ScanPageBottomBtn backgroundColor={'#51be85'} text={'出示渝康码首页'}/>
+        </Link>
+    )
+}
+
+function ScanPageBottomBtnOpenXingChengPopup(){
+    return(
+        <div className={'spb-open-xc-popup'}>
+            <ScanPageBottomBtn backgroundColor={'#5395f5'} text={'出示通信行程卡'}/>
+        </div>
+    )
+}
+
+function ScanPageBottomBtn(props){
+    let { backgroundColor, text} = props;
+    return(
+        <div className={'spb-btn'} style={{backgroundColor:backgroundColor}}>
+            <span>{text}</span>
+        </div>
+    )
+}
+
+function ScanPageCard(props){
+    let {setShowListFnc,position} = props;
+
+    let [time,setTime] = useState('');
+
+    function CheckTime(i){
+        if(i < 10){
+            i = '0' + i;
+        }
+        return i;
+    }
+
+    function GetTime(){
+        let d= new Date();
+        let year = d.getFullYear()
+        let month = d.getMonth() + 1
+        let date = d.getDate()
+        let h=d.getHours();
+        let m=d.getMinutes();
+        let s=d.getSeconds();
+        h=CheckTime(h);
+        m=CheckTime(m);
+        s=CheckTime(s);
+        let result = year + '-' + month + '-' + date + " " + h + ":" + m + ":" + s
+        setTime(result);
+    }
+
+    useEffect(()=>{
+        GetTime();
+    },[])
+
+    return(
+        <div className={'sp-card'}>
+            <div className={'sp-card-header'} onClick={()=>{ setShowListFnc(true) }}>
+                <div className={'scan-location-location'}>{position.location}</div>
+                <div className={'scan-location-area'}>重庆市/{position.area}</div>
+                <div className={'scan-location-road'}>{position.road}</div>
+            </div>
+            <div className={'sp-card-virtual-line'}>
+                <div className={'sp-card-virtual-line-circle-l'}/>
+                <div className={'sp-card-virtual-line-circle-r'}/>
+            </div>
+            <div className={'sp-card-detail'}>
+                <div className={'sp-card-detail-title'}>扫码结果</div>
+                <span className={'scan-time'}>{time}</span>
+                <ScanCardUserInfo/>
+                <img className={'check-24'} src={Check24}/>
+            </div>
+        </div>
+    )
+}
+
+function ScanCardUserInfo(){
+    let [username,setUsername] = useState('');
+    let [phone,setPhone] = useState('');
+    let [identity,setIdentity] = useState('');
+    useEffect(()=>{
+        let u = localStorage.getItem('username');
+        let p = localStorage.getItem('phone');
+        let i = localStorage.getItem('identity');
+        setUsername(u==null?"***":u);
+        setPhone(p==null?"188****7389":u);
+        setIdentity(i==null?"50*************50":u);
+
+    },[])
+    return(
+        <div className={'sc-user-info'}>
+            <div className={'sc-user-info-title'}>扫码用户信息</div>
+            <ScanCardUserEdit name={'手机'} value={username} setValue={()=>{}}/>
+            <ScanCardUserEdit name={'手机号码'} value={phone} setValue={()=>{}}/>
+            <ScanCardUserEdit name={'证件号码'} value={identity} setValue={()=>{}}/>
+        </div>
+    )
+}
+
+function ScanCardUserEdit(props){
+    let {name,value,setValue} = props;
+    return(
+        <div className={'sc-user-edit'}>
+            <span className={'sc-user-edit-key'}>{name}</span>
+            <span className={'sc-user-edit-value'}>{value}</span>
+            <img className={'sc-user-edit-show-icon'} src={showImg}></img>
+        </div>
     )
 }
 
